bf3e98848919eb85e7d1140cb6c179a1a11693e0
DarioArDi <darioartilesdiaz@gmail.com>
2024-05-30 14:51:35 +0100

    first commit

---
+++ Source/TeseoBros2/TeseoBros2.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TeseoBros2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TeseoBros2, "TeseoBros2" );
+ 
